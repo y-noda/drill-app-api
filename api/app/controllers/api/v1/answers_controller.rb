7b592f0d27a5ac2444bbf4997a8467263e7a35ad
y-noda <y-noda@entaworks.co.jp>
2021-07-29 18:50:03 +0900

    [add]簡易なapi機能を実装

---
+++ api/app/controllers/api/v1/answers_controller.rb
@@ -0,0 +1,33 @@
+class Api::V1::AnswersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  def create
+    
+    hash = JSON.parse(params[:answer])
+    key = hash["session"]["userid"]
+
+    @answer = Answer.find_or_initialize_by(key: key)
+
+    if @answer.new_record?
+      if @answer = Answer.create(key: key, answer: hash)
+        render status: 200, json: { id: key }
+      else
+        render status: 400, json: { id: '失敗' }
+      end
+    else
+      if @answer.update(key: key, answer: hash)
+        render status: 200, json: { id: key }
+      else
+        render status: 400, json: { id: '失敗' }
+      end
+    end
+  end
+
+  def show 
+    @answer = Answer.find_by(key: params[:id])
+    if @answer 
+      render status: 200, json: { answer: @answer.answer }
+    else
+      render status: 400, json: { answer: '失敗' }
+    end
+  end
+end
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   resources :venues
   resources :plans
+  namespace 'api' do
+    namespace 'v1' do
+      resources :answers
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
