386b31ae77a86913e6b364fdf97e4c751494492b
y-noda <y-noda@entaworks.co.jp>
2021-09-15 18:10:30 +0900

    [fix]年数のロジックを固めた

--- api/app/classes/save_answer.rb
+++ api/app/classes/save_answer.rb
@@ -2,11 +2,13 @@ class SaveAnswer
   attr_accessor :set_data
   attr_accessor :parameters
   attr_accessor :book_id
+  attr_accessor :sent_year
   attr_accessor :sent_month
 
   def initialize(init_data, parameters, book_id)
     @set_data = init_data
     @parameters = parameters
+    @sent_year = parameters[:dateStart].to_date.year
     @sent_month = parameters[:dateStart].to_date.month
     @book_id = book_id
   end
@@ -24,6 +26,7 @@ class SaveAnswer
     set_data[book_id][:answeredQuestionNum][:monthlyArr].push(parameters[:answeredQuestionNum])
 
     #最終更新月
+    set_data[book_id][:set_year] = sent_year
     set_data[book_id][:set_month] = sent_month
   end
 
@@ -34,29 +37,33 @@ class SaveAnswer
     set_data[book_id][:studyingTime][:total] += parameters[:elapsedTime]
     #回答数
     set_data[book_id][:answeredQuestionNum][:total] += parameters[:answeredQuestionNum]
+
+    
     #最終更新月
-    set_month = set_data[book_id][:set_month]
-    #集計
-    counting(set_month)
+    set_year = set_data[book_id][:set_year] 
+    set_month = set_data[book_id][:set_month] 
+    
+    #集計のadd
+    counting(set_year, set_month)
   end
 
   protected 
-    def counting(set_month)
+    def counting(set_year, set_month)
       
-      if sent_month == set_month
+      if sent_year == set_year && sent_month == set_month 
         #時間
         set_data[book_id][:studyingTime][:monthlyArr][-1] += parameters[:elapsedTime]
         #回答数
         set_data[book_id][:answeredQuestionNum][:monthlyArr][-1] += parameters[:answeredQuestionNum]
       else
         #久しぶりの送信なら0時間をpushして埋める
-        push_zero(set_month)
-        
+        fill_gap(set_year, set_month)
         #時間
         set_data[book_id][:studyingTime][:monthlyArr].push(parameters[:elapsedTime])
         #回答数
         set_data[book_id][:answeredQuestionNum][:monthlyArr].push(parameters[:answeredQuestionNum])
-        #最終更新月の更新
+        #最終履歴時間の更新
+        set_data[book_id][:set_year] = sent_year
         set_data[book_id][:set_month] = sent_month
 
         #12ヶ月分に調整
@@ -65,8 +72,41 @@ class SaveAnswer
       end
     end 
 
-    def push_zero(set_month)
-      gap_months = sent_month - set_month - 1
+    def fill_gap(set_year, set_month)
+
+      if sent_year == set_year && sent_month != set_month 
+      #月が違うだけ
+        if sent_month > set_month
+        # 正しい月順
+          gap_months = sent_month - set_month - 1
+          push_zero(gap_months)
+        else 
+        #異常な月順はリセット
+           #時間
+           set_data[book_id][:studyingTime][:monthlyArr] = []
+           #回答数
+           set_data[book_id][:answeredQuestionNum][:monthlyArr] = []
+        end
+
+      else
+      #年が異なる
+        year_gap = sent_year - set_year
+        if sent_year < set_year || sent_month >= set_month || year_gap > 1
+        #異常な年順または一年以上たってる場合はリセット
+          #時間
+          set_data[book_id][:studyingTime][:monthlyArr] = []
+          #回答数
+          set_data[book_id][:answeredQuestionNum][:monthlyArr] = []
+        elsif sent_month < set_month 
+        # 一年以内の場合
+          gap_months = (sent_month - 1) +  (12 - set_month) 
+          push_zero(gap_months)
+        end
+        
+      end
+    end
+
+    def push_zero(gap_months)
       gap_months.times do |i|
         #時間
         set_data[book_id][:studyingTime][:monthlyArr].push(0)
