32f83e4b8d30c8513faa3e1855517bfd404ef0de
y-noda <y-noda@entaworks.co.jp>
2021-09-21 11:50:05 +0900

    [add]log画面のrootingを分けた

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
       resources :answers
+      get 'log/:user_id', to: 'mypages#log'
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
