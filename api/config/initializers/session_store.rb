9c1b147d3847ca9169d1507f5d906595e4a6dee9
y-noda <y-noda@entaworks.co.jp>
2021-10-01 13:52:53 +0900

    [fix]ドメイン追加

--- api/config/initializers/session_store.rb
+++ api/config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production'
   Rails.application.config.session_store :cookie_store, key: '_drill-app-api', domain: 'test.flak.jp'
 else
-  Rails.application.config.session_store :cookie_store, key: '_drill-app-api'
+  Rails.application.config.session_store :cookie_store, key: '_drill-app-api', domain: 'test.flak.jp'
 end
