d5b253fec1521ee91b1f1c61481a056385148a27
y-noda <y-noda@entaworks.co.jp>
2021-10-07 09:09:31 +0900

    [fix] ユーザが持ってない時はダミー

--- api/app/controllers/api/v1/books_controller.rb
+++ api/app/controllers/api/v1/books_controller.rb
@@ -1,45 +1,67 @@
 class Api::V1::BooksController < ApplicationController
 
-  require './app/dummys/book_dummy'
-
   def user_books
+    require './app/dummys/book_dummy'
 
     user_id = params[:user_id]
 
     @answer = Answer.find_by(key: user_id)
-    books_data = @answer[:save_data]
-    book_keys = books_data.keys
 
-    crown_list = {}
+    if @answer
+      books_data = @answer[:save_data]
+      book_keys = books_data.keys
 
-    book_keys.each do |book_key|
-      crown = { gold: 0, silver: 0, bronze: 0 }
+      crown_list = {}
 
-      u_keys = books_data[book_key][:units].keys
+      book_keys.each do |book_key|
+        crown = { gold: 0, silver: 0, bronze: 0 }
 
-      u_keys.each do |u_key|
-        crown_list[u_key] = books_data[book_key][:units][u_key][:crown]
+        u_keys = books_data[book_key][:units].keys
+
+        u_keys.each do |u_key|
+          crown_list[u_key] = books_data[book_key][:units][u_key][:crown]
+        end
       end
-    end
 
-    dummy_books = call_books
+      dummy_books = call_books
 
-    book_keys.each do |book_key|
-      dummy_books.each_with_index do |dummy_book, index|
-        if dummy_book[:drillid] == book_key
-          units = dummy_book[:units]
-          units.each_with_index do |unit, unit_index|
-            if crown_list[unit[:id]]
-              dummy_books[index][:units][unit_index][:crown] = crown_list[unit[:id]]
+      book_keys.each do |book_key|
+        dummy_books.each_with_index do |dummy_book, index|
+          if dummy_book[:drillid] == book_key
+            units = dummy_book[:units]
+            units.each_with_index do |unit, unit_index|
+              if crown_list[unit[:id]]
+                dummy_books[index][:units][unit_index][:crown] = crown_list[unit[:id]]
+              end
             end
           end
         end
       end
+
+      output = dummy_books
+
+      render status: 200, json: output
+    else
+      output = [
+        { 
+          "drillid": "",
+          "grade": "",
+          "school": "",
+          "subject": "",
+          "title": "",
+          "img": "",
+          "units":[
+            {
+              "id": "",
+              "title": "",
+              "crown": ""
+            }
+          ]
+        }
+      ]
+
+      render status: 200, json: output
     end
-
-    output = dummy_books
-
-    render status: 400, json: output
   end
   
 end
