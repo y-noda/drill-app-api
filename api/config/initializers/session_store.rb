eab0750dc329d5272b78a84bc0530a3f95433931
Kazutaka Ueno <k-ueno@entaworks.co.jp>
2021-12-21 19:17:25 +0900

    update: ドメイン変更

--- api/config/initializers/session_store.rb
+++ api/config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production'
-  Rails.application.config.session_store :cookie_store, key: '_drill-app-api', domain: '40.74.136.199'
+  Rails.application.config.session_store :cookie_store, key: '_drill-app-api', domain: 'dev.ds-drill.com'
 else
-  Rails.application.config.session_store :cookie_store, key: '_drill-app-api', domain: '40.74.136.199'
+  Rails.application.config.session_store :cookie_store, key: '_drill-app-api', domain: 'test.ds-drill.com'
 end
